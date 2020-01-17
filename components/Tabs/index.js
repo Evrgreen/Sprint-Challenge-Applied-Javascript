73df5e631e9d3b62637f9067e9a8812689a9e5b9
Evrgreen <rcarsten06@gmail.com>
2020-01-17 09:55:49 -0700

    Added{tabs/index.js(axios call and tab generation script)}

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,16 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    console.log(response);
+    console.log(
+      response.data.topics.forEach(tab => {
+        let newTab = document.createElement("div");
+        newTab.classList.add("tab");
+        newTab.textContent = tab;
+        document.querySelector(".topics").append(newTab);
+      })
+    );
+  });
